1d58510fc611794ee5094defc99c6c80359443e5
Jake He <jake.he@visicase.com>
2018-03-26 11:05:19 +1100

    new readme, new product load

--- MENA/Bundle/MENALoadDataBundle/Migrations/Data/ORM/data/convertimages.sh
+++ MENA/Bundle/MENALoadDataBundle/Migrations/Data/ORM/data/convertimages.sh
@@ -11,4 +11,4 @@ find $1 -type f -not -name "*.jpg" -exec mogrify -format jpg {} +
 #remove all non jpg images
 find $1 -type f -not -name "*.jpg" -exec rm -rf {} +
 #reduce jpg image quality by 60%
-#find $1 -type f -name "*.jpg" -exec convert {} -sampling-factor 4:2:0 -strip -quality 60 -interlace JPEG -colorspace sRGB {} \;
+find $1 -type f -name "*.jpg" -exec convert {} -sampling-factor 4:2:0 -strip -quality 60 -interlace JPEG -colorspace sRGB {} \;
